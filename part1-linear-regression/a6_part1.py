9e91773f6d24ec0f99d2c9a8df1eb84de8423a6c
danielaaaq <dmolina12@cps.edu>
2024-12-05 10:36:59 -0600

    finished part 1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -11,14 +11,26 @@ y = data["Blood Pressure"].values
 x = x.reshape(-1,1)
 
 # Create the model
-
+model = LinearRegression().fit(x,y)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)  
 
-
+prediction = model.predict([[43]])
+print () 
 # Print out the linear equation and r squared value
-
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
 
 # Create the model in matplotlib and include the line of best fit
+plt.figure(figsize = (6,4)) 
+plt.scatter(x,y, c= 'purple')
+plt.scatter(43,prediction, c = 'blue')
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
